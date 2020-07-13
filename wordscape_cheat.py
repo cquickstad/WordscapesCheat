f8a684d3171fabef728ecc9b66a4d46e3f9f227c
Chad Quickstad <chad@haswell.quickstad.net>
2020-07-12 20:58:40 -0700

    Adding minimum word size command line switch.

--- wordscape_cheat.py
+++ wordscape_cheat.py
@@ -6,17 +6,15 @@ from enchant import Dict  # Dictionary to check for words
 from itertools import permutations  # Find all combinations of letters in a string
 
 
-MIN_WORD_SIZE = 3
-
-
 def parse_arguments():
     ap = ArgumentParser(description="Wordscape Cheat Tool")
+    ap.add_argument('-n', type=int, default=3, help="Minmum number of letters for shortest words")
     ap.add_argument('letters', nargs=1, type=str, default="", help="Letters presented in Wordscape puzzle")
     return ap.parse_args()
 
 
-def wordscape_permutations_generator(letters):
-    for n in range(MIN_WORD_SIZE, len(letters) + 1):
+def wordscape_permutations_generator(min_size, letters):
+    for n in range(min_size, len(letters) + 1):
         for p in permutations(letters, n):
             yield "".join(p)
 
@@ -25,9 +23,9 @@ if __name__ == '__main__':
     args = parse_arguments()
     dict = Dict('en_US')
     letters = ''.join(args.letters)
-    assert len(letters) >= MIN_WORD_SIZE
+    assert len(letters) >= args.n
     answers = set()
-    for permutation_of_letters in wordscape_permutations_generator(letters):
+    for permutation_of_letters in wordscape_permutations_generator(args.n, letters):
         if dict.check(permutation_of_letters):
             answers.add(permutation_of_letters)
 
